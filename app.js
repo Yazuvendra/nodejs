17d45a1d361e48bfcc996396728d644749fa0ede
Yazuvendra <ytirvoipati@allianceglobalservices.com>
2016-03-21 22:20:34 +0530

    asddd

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var requestListener = function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello You\n');
+}
+var server = http.createServer(requestListener);
+server.listen(8080);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
 });
